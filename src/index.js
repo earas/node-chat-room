ce8fae0f7e61796eacd28427295feb36fdc59750
nazifcand <nazifcandurgutt@gmail.com>
2020-11-15 20:45:55 +0300

    utilsları düzenledim

--- src/index.js
+++ src/index.js
@@ -16,72 +16,72 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
-    console.log('New WebSocket connection')
+  console.log('New WebSocket connection')
 
-    socket.on('join', (options, callback) => {
-        const { error, user } = addUser({ id: socket.id, ...options })
+  socket.on('join', (options, callback) => {
+    const { error, user } = addUser({ id: socket.id, ...options })
 
-        if (error) {
-            return callback(error)
-        }
+    if (error) {
+      return callback(error)
+    }
 
-        socket.join(user.room)
+    socket.join(user.room)
 
-        socket.emit('message', generateMessage('Admin', 'Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
-        io.to(user.room).emit('roomData', {
-            room: user.room,
-            users: getUsersInRoom(user.room)
-        })
-
-        callback()
+    socket.emit('message', generateMessage('Admin', 'Welcome!'))
+    socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room)
     })
 
-    socket.on('sendMessage', (message, callback) => {
-        const user = getUser(socket.id)
-        const filter = new Filter()
+    callback()
+  })
 
-        if (filter.isProfane(message)) {
-            return callback('Profanity is not allowed!')
-        }
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id)
+    const filter = new Filter()
 
-        io.to(user.room).emit('message', generateMessage(user.username, message))
-        //socket.broadcast.to(user.room).emit('typing', user.username)
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed!')
+    }
 
-        callback()
-    })
+    io.to(user.room).emit('message', generateMessage(user.username, message))
+    //socket.broadcast.to(user.room).emit('typing', user.username)
 
-    socket.on('sendLocation', (coords, callback) => {
-        const user = getUser(socket.id)
-        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
-        //socket.broadcast.to(user.room).emit('untyping', user.username)
-        callback()
-    })
+    callback()
+  })
+
+  socket.on('sendLocation', (coords, callback) => {
+    const user = getUser(socket.id)
+    io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+    //socket.broadcast.to(user.room).emit('untyping', user.username)
+    callback()
+  })
 
 
-    socket.on('typing', () => {
-        const user = getUser(socket.id)
-        socket.broadcast.to(user.room).emit('typing', user.username)
-    })
+  socket.on('typing', () => {
+    const user = getUser(socket.id)
+    socket.broadcast.to(user.room).emit('typing', user.username)
+  })
 
-    socket.on('untyping', () => {
-        const user = getUser(socket.id)
-        socket.broadcast.to(user.room).emit('untyping', user.username)
-    })
+  socket.on('untyping', () => {
+    const user = getUser(socket.id)
+    socket.broadcast.to(user.room).emit('untyping', user.username)
+  })
 
-    socket.on('disconnect', () => {
-        const user = removeUser(socket.id)
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id)
 
-        if (user) {
-            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
-            io.to(user.room).emit('roomData', {
-                room: user.room,
-                users: getUsersInRoom(user.room)
-            })
-        }
-    })
+    if (user) {
+      io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      })
+    }
+  })
 })
 
 server.listen(port, () => {
-    console.log(`Server is up on port ${port}!`)
+  console.log(`Server is up on port ${port}!`)
 })
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,20 +1,15 @@
-const generateMessage = (username, text) => {
-    return {
-        username,
-        text,
-        createdAt: new Date().getTime()
-    }
+exports.generateMessage = (username, text) => {
+  return {
+    username,
+    text,
+    createdAt: new Date().getTime()
+  }
 }
 
-const generateLocationMessage = (username, url) => {
-    return {
-        username,
-        url,
-        createdAt: new Date().getTime()
-    }
-}
-
-module.exports = {
-    generateMessage,
-    generateLocationMessage
+exports.generateLocationMessage = (username, url) => {
+  return {
+    username,
+    url,
+    createdAt: new Date().getTime()
+  }
 }
